2b631850bbe58f0a787df50598d7fdc18ef86d98
Gaurav Thapa <gaurav16547@gmail.com>
2023-05-15 22:46:21 +0545

    logic for countdown and audio play when time is over is added

---
+++ app.js
@@ -0,0 +1,44 @@
+const app = () => {
+    let startButton = document.getElementById("startbutton");
+    startButton.addEventListener('click', workTimer);
+
+    let minutes = document.querySelector(".minutes");
+    let seconds = document.querySelector(".seconds");
+    let restStart = document.getElementById("restStart");
+    let restOver = document.getElementById("restOver");
+
+    function workTimer() {
+        minutes.textContent = "20";
+        seconds.textContent = "10";
+        let intervalReturn = setInterval(() => {
+            if (seconds.textContent != 0) {
+                seconds.textContent -= 1;
+            }
+            else if (minutes.textContent != 0 && seconds.textContent == 0) {
+                minutes.textContent -= 1;
+                seconds.textContent = 59;
+            }
+            else {
+                clearInterval(intervalReturn);
+                restStart.play();
+                restTimer();
+            }
+        }, 1000);
+
+    }
+    function restTimer() {
+        seconds.textContent = "25";
+        minutes.textContent = "0";
+        let intervalReturn = setInterval(() => {
+            if (seconds.textContent != "0") {
+                seconds.textContent -= 1;
+            }
+            else {
+                restOver.play();
+                clearInterval(intervalReturn);
+                workTimer();
+            }
+        }, 1000);
+    }
+}
+app();
